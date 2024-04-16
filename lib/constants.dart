380a0a55bcaebadc211de4c4a6ed515c5642e2e8
LifeIsOne <didtmdgh258@gmail.com>
2024-04-16 14:46:45 +0900

    HomePage메서드 작성(사진, 앱바)

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'dart:ui';
+
+const kAccentColor = Color(0xFFF385C);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
+      home: Scaffold( // HomePage() 메서드
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: Text("앱바"),
+          backgroundColor: Colors.transparent,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/background.jpeg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
